15e4308a7d7249cc49ddecd4e8f6628172252305
krs1bbh <Stefan.Krause3@de.bosch.com>
2021-02-19 16:27:00 +0100

    [test] add sample data and adjust test.py to run with them

--- ImportFilter.py
+++ ImportFilter.py
@@ -11,18 +11,21 @@ class Importfile:
     
     def __init__(self, equipment='', product='', recipe='', path_data=''):
         """
-        
+        Initialization of the Importfile class.
 
         Parameters
         ----------
-        equipment : TYPE
-            DESCRIPTION.
-        product : TYPE
-            DESCRIPTION.
-        recipe : TYPE
-            DESCRIPTION.
-        path_data : TYPE
-            DESCRIPTION.
+        equipment : STRING
+            Describes equipment from which the data comes. The default is ''.
+            Implemented to far:
+                - NuK - Rudolph n&k equipment
+                - SmV - SmartView equipment
+        product : STRING
+            Product which the data were recorded of. The default is ''.
+        recipe : STRING
+            Manufactoring recipe of process flow. The default is ''.
+        path_data : STRING
+            Related data path. The default is ''.
 
         Returns
         -------
@@ -38,12 +41,12 @@ class Importfile:
                
     def export(self, path_out = ''):
         """
-        
+        Export of class objects data into excel file in given path.
 
         Parameters
         ----------
-        path_out : TYPE, optional
-            DESCRIPTION. The default is ''.
+        path_out : STRING, optional
+            Export path in which excel file is saved. The default is ''.
 
         Returns
         -------
@@ -56,20 +59,12 @@ class Importfile:
     
     def read_data(self):
         """
-        
-
-        Parameters
-        ----------
-        delimiter : TYPE, optional
-            DESCRIPTION. The default is ','.
-        decimal : TYPE, optional
-            DESCRIPTION. The default is '.'.
-        header : TYPE, optional
-            DESCRIPTION. The default is 0.
+        Reads data depending on equipment type and saves it into objects data.
 
         Returns
         -------
-        None.
+        TYPE
+            DESCRIPTION.
 
         """
         if self.equipment=='NuK':
@@ -87,15 +82,47 @@ class Importfile:
         self.data['file']=self.path_data
     
     def _read_nuk_data(self):
+        """
+        Data import for NuK equipment.
+
+        Returns
+        -------
+        None.
+
+        """
         self.data=pd.read_csv(self.path_data, delimiter=',', decimal='.', header=0)
     
     def _read_smv_data(self):
+        """
+        Data import for SmV equipment.
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.data=pd.read_csv(self.path_data, delimiter="\t", skiprows=54, header=None,na_values='-', names=['MP', 'Position','Membrandicke','Kavernentiefe','qual','bin'], dtype={'Position':'object','d1':'float64','d2':'float64','Qual':'float64'})
     
     def _process_raw_data_smv(self):
+        """
+        Transfer of header information into separate columns and creation of standard columns "m_date", "lot_id", "wafer_id", "product", and "recipe".
+        There is also a couple of operations in there:
+            - Calculate measurement position from col, row, DieSizeX, and DieSizeY as well as a radius r
+            - Transfer "StartTime" into datetime format
+            - Transfer wafer id into standard format XXXXXX-YY            
+        
+        Returns
+        -------
+        None.
+
+        """
+        #extract header from file
         header=pd.read_csv(self.path_data, delimiter='=|/',engine='python', decimal='.', nrows=23, comment='[', header=None, encoding='latin-1', index_col=False);
+        #convert header information in individual columns
         for i in range(len(header[0])):
             self.data[header[0][i]]=header[1][i]
+        #create and calculate columns described above
         self.data = self.data.rename(index=str, columns={"BatchID":"lot_id"})
         self.data['wafer_id']=header.iloc[13,1][:-2]+'-'+header.iloc[13,1][-2:]
         self.data.drop(columns='WaferID',inplace=True)
@@ -109,6 +136,18 @@ class Importfile:
         self.data.drop(columns=['Position'], inplace=True)
         
     def _process_raw_data_nuk(self):
+        """
+        Creation of standard columns "m_date", "lot_id", "wafer_id", "product", and "recipe".
+        There is also a couple of operations in there:
+            - Calculate radius r from x and y
+            - Transfer "DateTime" into datetime format
+            - Transfer wafer id into standard format XXXXXX-YY  
+
+        Returns
+        -------
+        None.
+
+        """
         new = self.data['Position'].str.split(",|=", n=4, expand=True)
         self.data['x']=new[1].astype('float')*10
         self.data['y']=new[3].astype('float')*10  
